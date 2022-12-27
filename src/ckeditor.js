827d35196183aa1f38c53c0abc5d9c3fd0626f4d
SviatLC <105208516+SviatLC@users.noreply.github.com>
2022-12-27 10:30:28 +0100

    test

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -13,8 +13,6 @@ import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
-import CloudServices from "@ckeditor/ckeditor5-cloudservices/src/cloudservices";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
 import Image from "@ckeditor/ckeditor5-image/src/image";
 import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
@@ -50,8 +48,6 @@ ClassicEditor.builtinPlugins = [
 	Italic,
 	BlockQuote,
 	CKFinder,
-	CloudServices,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
